dbb4573da4ead1396e729bae002a652b48e76908
oecorrechag <oecorrechag@unal.edu.co>
2023-02-05 10:25:33 -0500

    se agrego el header con dbc y se modifico el footer con 4 de padding

--- Dash/page_platzi/layouts.py
+++ Dash/page_platzi/layouts.py
@@ -1,13 +1,49 @@
 from dash import html
 import dash_bootstrap_components as dbc
 
-header = html.Div([
-    html.H1('Hello Dash'),
-    html.Div([
-        html.P('Dash converts Python classes into HTML'),
-        html.P("This conversion happens behind the scenes by Dash's JavaScript front-end")
-        ]),
-    ])
+LOGO = "assets\logo.png"
+TITLE = 'Project Name'
+
+header = html.Nav([
+            dbc.Navbar(
+                dbc.Container(
+                    [
+                        html.A(
+                            dbc.Row(
+                                [
+                                    dbc.Col(html.Img(src=LOGO, height="30px")),
+                                    dbc.Col(dbc.NavbarBrand(TITLE, className="ms-2")),
+                                ],
+                                align="center",
+                                className="g-0",
+                            ),
+                            href="/",
+                            style={"textDecoration": "none"},
+                        ),
+                        dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+                        dbc.Collapse(className="navbar", children=[
+
+                            dbc.NavItem(dbc.NavLink(html.A("Home", href="/home",
+                                            className='text-light font-weight-bold'))),
+
+                            dbc.NavItem(dbc.NavLink(html.A("Page1", href="/page1",
+                                            className='text-light font-weight-bold'))),
+
+                            dbc.NavItem(dbc.NavLink(html.A("Page2", href="/page2",
+                                            className='text-light font-weight-bold'))),
+
+                            dbc.NavItem(dbc.NavLink(html.A("Tickets", id='modal_comprar', href="/",
+                                            className='text-platzi font-weight-bold'))),
+
+                        ], id="navbar-collapse", is_open=False, navbar=True,
+                        ),
+                    ]
+                ),
+                color="dark",
+                dark=True,
+            ), 
+
+    ], className='fixed-top'),
 
 footer = dbc.Container([
             dbc.Row(children=[
@@ -16,4 +52,4 @@ footer = dbc.Container([
                 html.A('Oscar', href='https://github.com/oecorrechag', target="_blank"),
 
             ], className="row text-center"), 
-        ], fluid=True, id="footer", className="pb-2 pt-2"),
+        ], fluid=True, id="footer", className="pb-4 pt-4"),
