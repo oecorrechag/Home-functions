699b82992992aa812135c62445a3425e211fcb7d
oecorrechag <oecorrechag@unal.edu.co>
2023-02-07 22:37:08 -0500

    agregue el modal

--- Dash/page_platzi/components/CallbacksHeader.py
+++ Dash/page_platzi/components/CallbacksHeader.py
@@ -1,6 +1,8 @@
 from dash import html, Input, Output, State, callback
 import dash_bootstrap_components as dbc
 
+from utils.consts import GITHUB_PROFILE, LINKEDIN_PROFILE
+
 
 modal = html.Div(
     [
@@ -8,10 +10,20 @@ modal = html.Div(
         dbc.Modal(
             [
                 dbc.ModalHeader(dbc.ModalTitle("Codigo y redes sociales")),
-                dbc.ModalBody("This is the content of the modal"),
+
+                dbc.Row(className="m-3", children=[
+                        dbc.Col(className="col-4 offset-4 center-text", children=[
+
+                            html.A(html.Img(src='assets\icons\github.png', width="80px", height="80px"), id='moving', href=GITHUB_PROFILE),
+                            html.A(html.Img(src='assets\icons\linkedin.png', width="100px", height="100px"), href=LINKEDIN_PROFILE),
+
+                        ]),
+                    ],
+                ),
+
                 dbc.ModalFooter(
                     dbc.Button(
-                        "Close", id="close", className="ms-auto", n_clicks=0
+                        "Cancelar", id="close", color="secondary", className="ms-auto", n_clicks=0
                     )
                 ),
             ],
---
+++ Dash/page_platzi/utils/consts.py
@@ -0,0 +1,3 @@
+
+GITHUB_PROFILE = "https://github.com/oecorrechag"
+LINKEDIN_PROFILE = "https://www.linkedin.com/in/oscar-correcha/"
