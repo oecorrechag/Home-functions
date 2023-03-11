cd269fdcc06813b8cd651452d21404a83205090b
oecorrechag <oecorrechag@unal.edu.co>
2023-03-11 12:53:43 -0500

    creaccion de dataframe from list

--- crear_dataframes/dataframe.py
+++ crear_dataframes/dataframe.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+###### Dataframe desde diccionario
+
 df1 = {'integrantes': ['A','B','C','D','E'], 
        'valor': [1,np.nan,np.nan,np.nan,np.nan],
        'car': [1,2,4,5,6],
@@ -12,4 +14,16 @@ df1
 
 df = pd.DataFrame(columns=['A','B','C','D','E','F','G'])
 df.insert(0, "id", [1,2,3,4,5])
+df.head()
+
+###### Dataframe desde listas
+
+# list of strings
+lst = ['A','B','C','D','E']
+# list of int
+lst2 = [11, 22, 33, 44, 55]
+  
+# Calling DataFrame constructor after zipping
+df = pd.DataFrame(list(zip(lst, lst2)),
+               columns =['letter', 'val'])
 df.head()
