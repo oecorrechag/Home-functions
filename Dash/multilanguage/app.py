10eb9c590c7d1c208a9d5f90bf8589cf28fc6d92
oecorrechag <oecorrechag@unal.edu.co>
2023-04-08 14:41:37 -0500

    se modificaron los botones con dbc y el multilenguaje con otro input

--- Dash/multilanguage/app.py
+++ Dash/multilanguage/app.py
@@ -20,12 +20,6 @@ data_store = html.Div([dcc.Store(id="original_data", data=df.to_json()),
                        dcc.Store(id="filter_data"),
                        ])
 
-
-from dash import dcc
-languages = ['English', 'Español'] # lista de idiomas disponibles
-selected_language = 'English' # idioma seleccionado por defecto
-
-
 app = Dash(__name__, title = 'Page test',
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True
@@ -40,20 +34,27 @@ app.layout = dbc.Container([
     # Header
     html.Div(id='header'),
 
+    html.Div([
+     
+        dbc.Col(className='col-2 offset-10 pt=6', children=[
+
+            html.Br(),html.Br(),html.Br(),
+
+            dbc.Checklist(
+                options=[
+                    {"label": "English", "value": 1},
+                ],
+                value=[1],
+                id="select_language",
+                switch=True,
+            ),
+
+        ]),
+
+    ]),
+
     # Pagina
     html.Div(id='page-content'),
-
-    # Create Div to place a conditionally visible element inside
-    html.Div(id='slider-container', children=[
-
-        html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),html.Br(),
-        dcc.Dropdown(
-            options=[{'label': language, 'value': language} for language in languages],
-            value=selected_language,
-            id='language_dropdown'
-        ),
-
-    ], style= {'display': 'block'}), # <-- This is the line that will be changed by the dropdown callback
 
     # Footer
     html.Div(id='footer'),
@@ -71,15 +72,15 @@ def display_page(pathname):
 # Para las paginas
 @callback(Output('page-content', 'children'),
           Input('url', 'pathname'),
-          Input('language_dropdown', 'value'),
+          Input('select_language', 'value'),
           )
-def display_page(pathname, value):
-    print('value: ', value)
+def display_page(pathname, select_language):
+    # print('value: ', select_language)
     if (pathname == '/home') | (pathname == '/'):
          return home
-    elif (pathname == '/page1') & (value == 'English'):
+    elif (pathname == '/page1') & (select_language == [1]):
          return layout1en
-    elif (pathname == '/page1') & (value == 'Español'):
+    elif (pathname == '/page1') & (select_language == []):
          return layout1es
     elif pathname == '/page2':
          return layout2
