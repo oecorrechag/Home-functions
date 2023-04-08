fe13bf84c1104e1686a3e582e432ad4c9e90e1f7
oecorrechag <oecorrechag@unal.edu.co>
2023-04-08 16:31:20 -0500

    modificando graficos

---
+++ graficos/mapa full/app.py
@@ -0,0 +1,58 @@
+from dash import Dash, dcc, html, Input, Output, callback
+import pandas as pd
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+app = Dash(__name__, title = 'Grafico full',
+           external_stylesheets=[dbc.themes.BOOTSTRAP,dbc.icons.BOOTSTRAP,]
+)
+
+df = pd.read_csv("https://raw.githubusercontent.com/Coding-with-Adam/Dash-by-Plotly/master/Callbacks/chained_callback/social-capital-project.csv")
+
+app.layout = html.Div([
+
+    dcc.Dropdown(
+        id='states',
+        options=[{'label': 'ss', 'value': 'ss'}],
+        value='ss',
+        clearable=False
+    ),
+
+    html.Div(className="container-fluid", children=[
+        dcc.Graph(id='display-map', figure={}, style={'width': '99vw', 'height': '95vh'}),
+    ])
+
+])
+
+@callback(
+    Output('display-map', 'figure'),
+    Input('states', 'value')
+)
+def update_grpah(states):
+    dff = df.copy()
+
+    fig = px.scatter(dff, x='% without health insurance', y='% in fair or poor health',
+                    color='% adults graduated high school',
+                    trendline='ols',
+                    size='bubble_size',
+                    hover_name='County',
+                    # hover_data={'bubble_size':False},
+                    labels={'% adults graduated high school':'% graduated high school',
+                            '% without health insurance':'% no health insurance',
+                            '% in fair or poor health':'% poor health'}
+                    )
+    fig.update_layout(showlegend=False)
+    fig.update_layout(yaxis_visible=False, yaxis_showticklabels=False, 
+                      xaxis_visible=False, xaxis_showticklabels=False,
+                      margin=dict(l=0, r=0, t=0, b=0),
+                      showlegend=False, autosize=True,)
+    
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+    
+ 
+# https://youtu.be/ZxshFO0bbZM
