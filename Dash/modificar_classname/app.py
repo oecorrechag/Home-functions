dc3be1f8a2943320ec2578259743d1fde4da4882
oecorrechag <oecorrechag@unal.edu.co>
2023-04-12 15:15:01 -0500

    se agrego un efecto de acultar html

--- Dash/modificar_classname/app.py
+++ Dash/modificar_classname/app.py
@@ -9,24 +9,56 @@ server = app.server
 
 app.layout = html.Div([
 
-        dbc.Button("Cambiar tamaño", id="boton", color="success", className="me-1"),
+        html.Div(id='ocultar_html', children=[
+    
+            html.Br(),html.Br(),html.Br(),
+    
+        ], style= {'display': 'block'}),
+
+        dbc.Row([
+    
+            dbc.Col(className="bg-primary p-3 mt-3 col-12", children=[
+    
+                dbc.Button("Cambiar tamaño", id="boton", color="success", className="me-1"),
+
+                html.H6("Este es un ejemplo de texto"),
+
+            ]),
+
         
-        html.Div("Este es un ejemplo de texto", id="texto", className="bg-primary p-3 mt-3 col-12"),
+        ]),
+
+
 
 ])
 
-@app.callback(
-    Output("texto", "className"),
-    Input("boton", "n_clicks"),
-    State("texto", "className")
+# Esconder html
+@callback(
+    Output('ocultar_html', 'style'),
+    Input("boton", "n_clicks")
 )
-def cambiar_color(n_clicks, class_name):
-    if n_clicks and "col-6" in class_name:
-        return class_name.replace("col-6", "col-12")
-    elif n_clicks and "col-12" in class_name:
-        return class_name.replace("col-12", "col-6")
-    else:
-        return class_name
+def show_hide_element(n_clicks):
+    if (n_clicks is not None) and (n_clicks % 2 != 0):
+        return {'display': 'block'}
+    elif (n_clicks is not None) and (n_clicks % 2 == 0):
+        return {'display': 'none'}
+    elif n_clicks is None:
+        return {'display': 'none'}
+
+
+
+# @app.callback(
+#     Output("texto", "className"),
+#     Input("boton", "n_clicks"),
+#     State("texto", "className")
+# )
+# def cambiar_color(n_clicks, class_name):
+#     if n_clicks and "col-6" in class_name:
+#         return class_name.replace("col-6", "col-12")
+#     elif n_clicks and "col-12" in class_name:
+#         return class_name.replace("col-12", "col-6")
+#     else:
+#         return class_name
     
 if __name__ == '__main__':
     app.run_server(debug=True)
