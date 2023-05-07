9817aa7afb3d7ced74df7af346b0a614db37921f
oecorrechag <oecorrechag@unal.edu.co>
2023-05-06 23:19:03 -0500

    8 indicadores

--- Dash/indicators/app.py
+++ Dash/indicators/app.py
@@ -53,41 +53,62 @@ def func(value):
         mode = "number+delta",
         value = 100,
         title = {"text": " Indicator 1 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> Subsubtitle </span>"},
-        domain = {'x': [0.0, 0.2], 'y': [0.0, 0.5]},
+        domain = {'x': [0.0, 0.2], 'y': [0.5, 1.0]},
         delta = {'reference': 1000, 'relative': True, 'position' : "top"}))
 
     fig.add_trace(go.Indicator(
         mode = "number+delta",
         value = 1,
         title = {"text": " Indicator 2 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> left </span>"},
-        domain = {'x': [0.2, 0.4], 'y': [0.0, 0.5]},
+        domain = {'x': [0.2, 0.4], 'y': [0.5, 1.0]},
         delta = {'reference': 1000, 'relative': True, 'position' : "left"}))
 
     fig.add_trace(go.Indicator(
         mode = "number+delta",
         value = 523,
         title = {"text": " Indicator 3 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> bottom </span>"},
-        domain = {'x': [0.4, 0.6], 'y': [0.0, 0.5]},
+        domain = {'x': [0.4, 0.6], 'y': [0.5, 1.0]},
         delta = {'reference': 1000, 'relative': True, 'position' : "bottom"}))
 
     fig.add_trace(go.Indicator(
         mode = "number+delta",
         value = 5253,
         title = {"text": " Indicator 4 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> right </span>"},
-        domain = {'x': [0.6, 0.8], 'y': [0.0, 0.5]},
+        domain = {'x': [0.6, 0.8], 'y': [0.5, 1.0]},
         delta = {'reference': 1000, 'relative': True, 'position' : "right"}))
     
     fig.add_trace(go.Indicator(
         mode = "number+delta",
         value = 100000,
         title = {"text": " Indicator 5 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> top </span>"},
-        domain = {'x': [0.8, 1.0], 'y': [0.0, 0.5]},
+        domain = {'x': [0.8, 1.0], 'y': [0.5, 1.0]},
         delta = {'reference': 1000, 'relative': True, 'position' : "top"}))
 
 
 
+    fig.add_trace(go.Indicator(
+        mode = "number+delta",
+        value = 5423,
+        title = {"text": " Indicator 6 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> bottom </span>"},
+        domain = {'x': [0.0, 0.33], 'y': [0.0, 0.5]},
+        delta = {'reference': 99, 'relative': True, 'position' : "bottom"}))
+
+    fig.add_trace(go.Indicator(
+        mode = "number+delta",
+        value = 823,
+        title = {"text": " Indicator 7 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> bottom </span>"},
+        domain = {'x': [0.33, 0.66], 'y': [0.0, 0.5]},
+        delta = {'reference': 99, 'relative': True, 'position' : "bottom"}))
+    
+    fig.add_trace(go.Indicator(
+        mode = "number+delta",
+        value = 74,
+        title = {"text": " Indicator 8 <br><span style='font-size:0.8em;color:gray'> Subtitle </span><br><span style='font-size:0.8em;color:gray'> bottom </span>"},
+        domain = {'x': [0.66, 1.0], 'y': [0.0, 0.5]},
+        delta = {'reference': 99, 'relative': True, 'position' : "bottom"}))
 
 
+    fig.update_layout(width=1200, height=800)
 
 
 
