cfd529e60ae55db6b31c40c566fd6e6067c5229b
oecorrechag <oecorrechag@unal.edu.co>
2023-05-13 12:34:54 -0500

    Se agrega el puntero a un grafico de serie de tiempo

--- Dash/puntero/app.py
+++ Dash/puntero/app.py
@@ -3,8 +3,17 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
+import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+
+from funciones import hover_hover
+
+df = pd.DataFrame(dict(
+    x = [1, 3, 2, 4],
+    y = [1, 2, 3, 4]
+))
+
 
 # Definimos la aplicación de Dash
 app = dash.Dash(__name__)
@@ -14,37 +23,28 @@ prev_y = None
 
 # Definimos el layout de la aplicación
 app.layout = html.Div([
-    dcc.Input(
-        id='x-input',
-        type='number',
-        value=5
-    ),
-    dcc.Input(
-        id='y-input',
-        type='number',
-        value=10
-    ),
-    dcc.Graph(
-        id='graph',
-    ),
-    html.Div(id='hover-data')
+
+    dcc.Input(id='x-input', type='number', value=5),
+
+    dcc.Graph(id='graph2',),
+
+    html.Div(id='hover-data'),
+
 ])
 
-# Definimos el callback para construir el gráfico y obtener la posición del puntero
 @app.callback(
-    Output('graph', 'figure'),
+    Output('graph2', 'figure'),
     Output('hover-data', 'children'),
     Input('x-input', 'value'),
-    Input('y-input', 'value'),
-    Input('graph', 'hoverData')
+    Input('graph2', 'hoverData')
 )
-def update_graph(x, y, hoverData):
+def update_graph(x, hoverData):
 
     global prev_x, prev_y
 
-    df = px.data.iris()
-
-    fig = px.scatter(df, x='sepal_length', y='sepal_width', color='species', hover_data=['species'])
+    fig = px.line(df, x="x", y="y", title="Unsorted Input", markers=True, line_shape='spline') 
+    fig.update_traces(hoverinfo='none')
+        
     fig.update_layout(
         hovermode="closest",
         xaxis=dict(
@@ -65,47 +65,35 @@ def update_graph(x, y, hoverData):
         )
     )
 
-
-    ###
-
     if hoverData is not None:
 
         x_hover = hoverData['points'][0]['x']
         y_hover = hoverData['points'][0]['y']
         color_hover = hoverData['points'][0]['curveNumber']
+        # name = hoverData['points'][0]['customdata'][0]
+        print('ASDASD', x_hover, y_hover, color_hover)
 
-        print(hoverData['points'][0])
-
-        # Compara el valor actual de 'x' con el valor anterior
-        if x_hover == prev_x:
-            hover_info = f'Posición del puntero: x={x_hover}, y={y_hover}'
-            # fig.add_trace(go.Scatter(x=[x_hover], y=[y_hover], mode='markers', showlegend=False, marker=dict(color='gray', size=20)))
-            fig.add_trace(go.Scatter(x=[x_hover], y=[y_hover], mode='markers', showlegend=False, marker=dict(color=color_hover, size=10)))
-            fig.update_traces(
-                hovertemplate="<br>".join([
-                    "Sepal length: %{x}",
-                    "Sepal width: %{y}",
-                ])
-            )
-        else:
-            prev_x = x_hover
-            prev_y = y_hover
-            hover_info = f'Posición del puntero nuevo: x={x_hover}, y={y_hover}'
-            print(x_hover, y_hover, color_hover)
-            # fig.add_trace(go.Scatter(x=[x_hover], y=[y_hover], mode='markers', showlegend=False, marker=dict(color='gray', size=20)))
-            fig.add_trace(go.Scatter(x=[x_hover], y=[y_hover], mode='markers', showlegend=False, marker=dict(color=color_hover, size=10)))
-            fig.update_traces(
-                hovertemplate="<br>".join([
-                    "Sepal length: %{x}",
-                    "Sepal width: %{y}",
-                ])
-            )
+        hover_info = hover_hover(prev_x, prev_y, x_hover, y_hover)
+        fig.add_trace(go.Scatter(x=[x_hover], y=[y_hover], mode='markers', name="Spline", showlegend=False, marker=dict(color='blue', size=10)))
+        fig.update_traces(
+            hovertemplate="<br>".join([
+                "Sepal length: %{x}",
+                "Sepal width: %{y}",
+            ])
+        )
     else:
         hover_info = ''
 
-    ###
-
     return fig, hover_info
+
+
+
+
+
+
+
+
+
 
 # Ejecutamos la aplicación
 if __name__ == '__main__':
