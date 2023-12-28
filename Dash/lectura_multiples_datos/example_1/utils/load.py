1483b52d54e504a75d0e48dcc2c26cf28333f98c
oecorrechag <oecorrechag@unal.edu.co>
2023-12-27 22:34:12 -0500

    folder has been moved to another repository

--- Dash/lectura_multiples_datos/example_1/utils/load.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-data = {
-    "archivo1.csv": pd.read_csv("data/archivo1.csv"),
-    "archivo2.csv": pd.read_csv("data/archivo2.csv")
-}
--- Dash/upload csv/utils/consts.py
+++
@@ -1,5 +0,0 @@
-LOGO = "assets\logo.png"
-TITLE = 'Project Name'
-
-GITHUB_PROFILE = "https://github.com/oecorrechag"
-LINKEDIN_PROFILE = "https://www.linkedin.com/in/oscar-correcha/"
